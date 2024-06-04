aa95096245db612119cea06fa9077584d521bcad
Milena <milena.s.silva15@aluno.senai.br>
2024-06-04 16:37:42 -0300

    wifi config

--- semaforoprj.ino
+++ semaforoprj.ino
@@ -1,3 +1,20 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+//biblioteca para executar um serviço de HTTP, para publicar os arquivos html
+#include <WebServer.h>
+#include <string.h>
+
+//instancia
+WebServer server(80);
+
+//asteristico é um ponteiro da memória
+const char *ssid = "Wifi-MAMR";
+const char *password = "87654321";
+
+IPAddress local_IP(192,168,0,80);
+IPAddress gateway(192,168,0,1);
+IPAddress subnet(255,255,255,0);
+
 const int pinLed1 = 2; 
 const int pinLed2 = 4; 
 const int pinLed3 = 5; 
@@ -6,11 +23,20 @@ const int pinLed5 = 19;
 const int pinLed6 = 21; 
 
 int tempoAmarelo = 5000;
-int tempoVerde = 30000;
-int tempoVermelho = 35000;
+int tempoVerde = 20000;
+int tempoVermelho = 25000;
 
 
 void setup() {
+  Serial.begin(115200);
+  //configuração do Acess Point que irá ser fornecido pelo esp32
+  Serial.print("Configurando AP...");
+  Serial.println(WiFi.softAPConfig(local_IP,gateway,subnet)?"OK":"Erro");
+  Serial.print("Iniciando o WiFi....");
+  Serial.println(WiFi.softAP(ssid,password));
+  Serial.print("Endereço IP");
+  Serial.println(WiFi.softAPIP());
+
   pinMode(pinLed1, OUTPUT);
   pinMode(pinLed2, OUTPUT);
   pinMode(pinLed3, OUTPUT);
